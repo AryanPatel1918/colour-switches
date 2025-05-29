ec5d29ea1d2270200f84b89b3a4777334d9da78e
aryanpatel1918 <aryanpatel1918@gmail.com>
2025-05-29 16:24:15 -0400

    Add react functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
 import { useState } from 'react'
-import Switch from './components/Switch'
+import Light from './components/Light'
+import lightsData from './lightsData'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [lights, setLights] = useState(lightsData)
 
+  function toggle(id) {
+    setLights(prevLights => 
+      prevLights.map(light =>
+        light.id === id ? {...light, on: !light.on} : light
+      )
+    )
+  }
+  
+  function turnOnAll() {
+    setLights(prevLights =>
+      prevLights.map(light =>
+        ({...light, on: true})
+      )
+    )
+  }
 
+  function turnOffAll() {
+    setLights(prevLights =>
+      prevLights.map(light =>
+        ({...light, on: false})
+      )
+    )
+  }
+  
+  
 
   return (
-    <h1>Hello</h1>
+    <div className="app-container">
+
+      <div className="buttons-container">
+        <button className="turn-on-all-btn" onClick={turnOnAll}>Turn On All</button>
+        <button className="turn-off-all-btn" onClick={turnOffAll}>Turn Off All</button>
+      </div>
+
+      <div className="lights-container">
+        {lights.map(light => (
+          <Light key={light.id} id={light.id} colour={light.colour} on={light.on} toggle={toggle} />
+        ))}
+      </div>
+
+    </div>
   )
 }
