5320d530fa46f20be711d4d480fea7d5376776b5
aryanpatel1918 <aryanpatel1918@gmail.com>
2025-05-29 16:21:22 -0400

    Create light objects array

---
+++ src/lightsData.js
@@ -0,0 +1,49 @@
+export default [
+    {
+        id: 0,
+        colour: "red",
+        on: true
+    },
+
+    {
+        id: 1,
+        colour: "orange",
+        on: true
+    },
+
+    {
+        id: 2,
+        colour: "yellow",
+        on: true
+    },
+
+    {
+        id: 3,
+        colour: "green",
+        on: true
+    },
+
+    {
+        id: 4,
+        colour: "blue",
+        on: true
+    },
+
+    {
+        id: 5,
+        colour: "indigo",
+        on: true
+    },
+
+    {
+        id: 6,
+        colour: "violet",
+        on: true
+    },
+
+    {
+        id: 7,
+        colour: "white",
+        on: true
+    }
+]
