00bf4499d8e3ab51ef563e3240cd7f79ed87d0fa
aryanpatel1918 <aryanpatel1918@gmail.com>
2025-06-06 16:42:24 -0400

    Add box shadow when light is on

--- src/components/Light.jsx
+++ src/components/Light.jsx
@@ -3,7 +3,7 @@ export default function Light(props) {
     return (
         <div 
             className={`light ${props.on ? "on" : ""}`}
-            style={{ backgroundColor: props.colour }}
+            style={{ backgroundColor: props.colour, boxShadow: props.on ? `0 0 12px ${props.colour}` : "none" }}
             onClick={() => props.toggle(props.id)}>
         </div>
     )
